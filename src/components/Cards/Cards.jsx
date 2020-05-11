d093a4d84537eab62b6c920a78ccb1434c86919a
Akash kumar Barnwal <akashkumarbarnwal666@gmail.com>
2020-05-11 16:44:59 +0530

    Text changing Testingggg...

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -33,7 +33,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) =>{
                         <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
                          </Typography>
                     <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                    <Typography variant="body2">Number of Recovery during covid-19</Typography>
+                    <Typography variant="body2">Number of Recovery </Typography>
                 </CardContent>
             </Grid>
 
